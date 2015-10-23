49f15dd093cfcd2490004574c1774136b847263c
Gabriel Deleon <notprivategabe@gmail.com>
2015-10-23 13:00:18 -0400

    bugfixes to & renamed _get_folders() to _get_snort_interface_directories

--- unified2_cleanup.py
+++ unified2_cleanup.py
@@ -24,14 +24,14 @@ ch.setFormatter(formatter)
 # Add ch to logger
 logger.addHandler(ch)
 
-def _get_folders(snort_path='/var/log/snort'):
+def _get_snort_interface_directories(snort_path='/var/log/snort'):
     '''
     Returns list with absolute path of child folders (interfaces) within snort_path
     '''
     logger.debug('Path of snort_path: {0}'.format(snort_path))
     output = []
     for i in os.listdir(snort_path):
-        potential_path = '{0}/{1}'.format(snort,i))
+        potential_path = '{0}/{1}'.format(snort_path,i)
         logger.debug('Checking if {0} is a directory'.format(potential_path))
         if os.path.isdir(potential_path):
             logger.debug('{0} is a valid directory'.format(potential_path))
@@ -40,5 +40,5 @@ def _get_folders(snort_path='/var/log/snort'):
     return output
 
 # Debug Section~!
-check = _get_folders()
+check =_get_snort_interface_directories
 print check
