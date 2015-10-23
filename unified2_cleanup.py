2a145a3cf59b07a1b125ee9fb15ec02b0f014737
Gabriel Deleon <notprivategabe@gmail.com>
2015-10-23 13:32:54 -0400

    switched "stripping" to "splitting" in _get_epoch() debug message

--- unified2_cleanup.py
+++ unified2_cleanup.py
@@ -56,7 +56,7 @@ def _get_epoch(unified2_file):
     '''
     Strips off prefix of a unified2 file and gets the epoch time
     '''
-    logger.debug('Stripping file {0}'.format(unified2_file))
+    logger.debug('Splitting file {0}'.format(unified2_file))
     epoch = unified2_file.split('.')[1]
     logger.debug('Extracted epoch time "{0}" from {1}'.format(epoch, unified2_file))
     return epoch
