728e3fceddf344ae5f3d83eb88a687021edf7723
Gabriel Deleon <notprivategabe@gmail.com>
2015-10-23 13:19:14 -0400

    added function _get_unified2_file_list()

--- unified2_cleanup.py
+++ unified2_cleanup.py
@@ -39,6 +39,22 @@ def _get_snort_interface_directories(snort_path='/var/log/snort'):
             logger.debug('{0} added to list of unified2 paths'.format(potential_path))
     return output
 
+def _get_unified2_file_list(interface_path, unified2_prefix='snort-unified2'):
+    '''
+    Returns a list of files within an interface folder ex:dag0:32
+    '''
+    output = []
+    logger.debug('Checking contents of {0}'.format(interface_path))
+    for i in os.listdir(interface_path):
+        if i.startswith(unified2_prefix):
+            logger.debug('{0} matches unified2_prefix, appending to output')
+            output.append(i)
+    return output
+
+
+
 # Debug Section~!
 check =_get_snort_interface_directories
-print check
+uni_check = _get_unified2_file_list(check[0])
+print uni_check
+
